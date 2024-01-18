c07565aaf6cdd69994928ce877e8f48ef218b856
JoseJimenezGonzalez <josexisco1993@gmail.com>
2024-01-18 16:39:57 +0100

    commit bundle es null

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,9 +41,8 @@ android {
 dependencies {
 
     //Navigation
-    val nav_version = "2.5.3"
-    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
-    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
@@ -53,9 +52,9 @@ dependencies {
 
     implementation ("androidx.recyclerview:recyclerview:1.3.2")
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
